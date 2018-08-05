26d45bc09d9925fa954fb3eb6888c505fca6560d
nahr <nahr@neusoft.com>
2018-08-05 22:52:27 +0800

    [+]upload code

--- menuController.js
+++ menuController.js
@@ -1,9 +1,11 @@
 angular.module('lintCodeOffline')
-.controller('menuCtrl',['$scope',function ($scope) {
+.controller('menuCtrl',['$scope','$state',function ($scope,$state) {
   $scope.$watch('menuList',function () {
 
   },false);
   $scope.jumpTo = function (unique_name) {
-    $state.go('detail','')
-  }
+    $state.go('detail',{name:unique_name})
+  };
+  var myScroll = new IScroll('#wrapper', { scrollX: true, freeScroll: true });
+  document.addEventListener('touchmove', function (e) { e.preventDefault(); },false);
 }]);
